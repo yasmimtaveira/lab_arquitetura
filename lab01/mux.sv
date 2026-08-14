ce82eba93b145177fa6f7ec4342392ac9b4be004
Yasmim Taveira <yasmimtaveiralps@gmail.com>
2026-08-14 14:08:34 -0300

    modulo mux 4:1 (1 bit)

--- lab01/mux.sv
+++ lab01/mux.sv
@@ -1,12 +1,15 @@
 module mux
   (
     output logic f,
-    input  logic a, b, [1:0]sel
+    input  logic a, b, c, d, [1:0]sel
   ); 
 
-  and g1(f1, a, n_sel),
-      g2(f2, b, sel);
-  or  g3(f, f1, f2);
-  not g4(n_sel, sel);
+  and g1(f1, a, n_sel_1, n_sel_0),
+      g2(f2, b, n_sel_1, sel[0]),
+      g3(f3, c, sel[1], n_sel_0),
+      g4(f4, d, sel[1], sel[0]);
+  or  g5(f, f1, f2, f3, f4);
+  not g6(n_sel_0, sel[0]),
+      g7(n_sel_1, sel[1]);
 
 endmodule
